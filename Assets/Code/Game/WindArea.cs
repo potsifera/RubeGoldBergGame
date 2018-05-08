8c630569807281478746f0303fcd22038319f881
Grace <grace@gracemontoya.com>
2018-05-07 19:30:48 -0500

    adds a wind script

---
+++ Assets/Code/Game/WindArea.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindArea : MonoBehaviour {
+
+	public float strength;
+	public Vector3 direction;
+	private Rigidbody otherRB;
+	
+
+	private void OnTriggerEnter(Collider other)
+	{
+		otherRB = other.GetComponent<Rigidbody>();
+		otherRB.AddForce(transform.up * 200);
+	}
+
+}
